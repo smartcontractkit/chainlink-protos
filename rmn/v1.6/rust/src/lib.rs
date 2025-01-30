8c28bbc97f40ae868cc10f2d19c4e934de82f17d
Simon B.Robert <simon.robert@smartcontract.com>
2025-01-30 15:13:17 -0500

    Change import path for rmn rust protos

--- rmn/v1.6/rust/src/lib.rs
+++ rmn/v1.6/rust/src/lib.rs
@@ -1,4 +1,15 @@
-include!("keystore.rs");
-include!("rageproxy.rs");
-include!("rmn_offchain.rs");
-include!("offchainreporting3_config.rs");
+pub mod rageproxy_proto {
+    include!("rageproxy.rs");
+}
+
+pub mod offchain_proto {
+    include!("rmn_offchain.rs");
+}
+
+pub mod keystore_proto {
+    include!("keystore.rs");
+}
+
+pub mod offchainreporting3_config_proto {
+    include!("offchainreporting3_config.rs");
+}
