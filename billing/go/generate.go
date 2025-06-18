2414c0a225c4049e8aa83aeb28f4a421ffafdfcc
Jer Ahrens <ahrens@gmail.com>
2025-06-18 16:47:09 -0700

    credit reservations proto to replace billing proto

--- billing/go/generate.go
+++ billing/go/generate.go
@@ -1,3 +1,3 @@
 package billing
 
-//go:generate protoc --proto_path=../ --proto_path=../../workflows --go_out=./ --go_opt=Mevents/metering.proto=github.com/smartcontractkit/chainlink-protos/workflows/go/events --go_opt=module=github.com/smartcontractkit/chainlink-protos/billing/go --go-grpc_out=./ --go-grpc_opt=module=github.com/smartcontractkit/chainlink-protos/billing/go ../billing/v1/billing_service.proto
+//go:generate protoc --proto_path=../ --proto_path=../../workflows --go_out=./ --go_opt=Mevents/metering.proto=github.com/smartcontractkit/chainlink-protos/workflows/go/events --go_opt=module=github.com/smartcontractkit/chainlink-protos/billing/go --go-grpc_out=./ --go-grpc_opt=module=github.com/smartcontractkit/chainlink-protos/billing/go ../billing/v1/credit_reservation_message_types.proto ../billing/v1/credit_reservation_service.proto
