e3e7b8028e1abb1c99e8a0e5b3b8ca93f2c15795
Pavel <177363085+pkcll@users.noreply.github.com>
2025-06-25 12:45:58 -0400

    move workflows/common/v1/base_message_legacy.proto -> workflows/pb/base_message_legacy.proto
    Why?
    https://github.com/smartcontractkit/chainlink-protos/actions/runs/15882133443/job/44785011345
    
    Error: Multiple packages "common.v1,pb" detected within directory "common/v1".
    Error: Multiple packages "common.v1,pb" detected within directory "common/v1".
    Error: Files with package "pb" must be within a directory "pb" relative to root but were in directory "common/v1".
    Error: Package name "pb" should be suffixed with a correctly formed version, such as "pb.v1".

--- workflows/go/generate.go
+++ workflows/go/generate.go
@@ -11,4 +11,4 @@ package workflows
 //go:generate protoc --proto_path=../ --go_out=./ --go_opt=module=github.com/smartcontractkit/chainlink-protos/workflows/go ../workflows/v1/workflow_started.proto
 //go:generate protoc --proto_path=../ --go_out=./ --go_opt=module=github.com/smartcontractkit/chainlink-protos/workflows/go ../workflows/v1/workflow_status_changed.proto
 //go:generate protoc --proto_path=../ --go_out=./ --go_opt=module=github.com/smartcontractkit/chainlink-protos/workflows/go ../common/v1/base_message.proto
-// do not include deprecated workflows/common/v1/base_message_legacy.proto
+// do not include deprecated workflows/pb/base_message_legacy.proto
