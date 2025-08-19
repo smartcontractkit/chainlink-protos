de07e60c209b3345b44c7f0175c2b417d18b973e
Pavel <177363085+pkcll@users.noreply.github.com>
2025-08-18 20:44:17 -0400

    data-feeds: split bridge_statys.proto into multiple files

--- data-feeds/generate.go
+++ data-feeds/generate.go
@@ -1,3 +1,8 @@
 package data_feeds
 
-//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ./v1/bridge_status.proto
+//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ./bridge_status/v1/bridge_status_event.proto
+//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ./bridge_status/v1/job_info.proto
+//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ./bridge_status/v1/runtime_info.proto
+//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ./bridge_status/v1/metrics_info.proto
+//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ./bridge_status/v1/endpoint_info.proto
+//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ./bridge_status/v1/configuration_item.proto
